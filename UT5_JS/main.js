9a94b862e8297aecbbad85722c601c4d3eeb8d3c
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-01-23 13:51:21 +0100

    trabajos con formularios

---
+++ UT5_JS/main.js
@@ -0,0 +1,3 @@
+document.write("Patata<br>") //si esta tachado esta en desuso
+document.writeln("Boniato<br>") //writeln es la manera actual
+// codifico mis funciones, cargo mi codigo js para que se ejecute en la partedel codigo que yo quiero
